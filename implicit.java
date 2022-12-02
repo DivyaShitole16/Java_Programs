c2953798d789e4e0d1cb5ff7e31b15067a1ccc9e
DivyaShitole16 <114485356+DivyaShitole16@users.noreply.github.com>
2022-12-02 08:46:20 -0500

    Program on implicit type casting

---
+++ implicit.java
@@ -0,0 +1,12 @@
+class implicit
+{
+public static void main(String args [ ])
+{
+int x=7;
+long y=x;
+float z=y;
+System.out.println(&quot;Before conversion int value&quot;+x);
+System.out.println(&quot;After conversion long value&quot;+y);
+System.out.println(&quot;After conversion float value&quot;+z);
+}
+}
